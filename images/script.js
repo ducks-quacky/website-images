d97f171572b194b4771eb328d5e5372278f1e268
stuxfian <116228096+ducks-quacky@users.noreply.github.com>
2025-02-09 11:27:06 -0800

    Create script.js

---
+++ images/script.js
@@ -0,0 +1,19 @@
+// List of image file names (Manually added)
+const images = [
+    "image1.jpg",
+    "image2.jpg",
+    "image3.jpg",
+    "image4.jpg"
+];
+
+// Get the gallery div
+const gallery = document.getElementById("gallery");
+
+// Loop through images and add them to the gallery
+images.forEach(image => {
+    const imgElement = document.createElement("img");
+    imgElement.src = `images/${image}`; // Path to images folder
+    imgElement.alt = "Gallery Image";
+    imgElement.classList.add("gallery-img");
+    gallery.appendChild(imgElement);
+});
