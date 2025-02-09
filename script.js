f4e55a9ee19ed92beedc5925cd62b16fa7351f2c
stuxfian <116228096+ducks-quacky@users.noreply.github.com>
2025-02-09 12:01:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-// List of image filenames
-const images = [
-    "image1.avif",
-    "mN1VKT3WvGehYiKDHY8n4BU.avif",
-    "q1gHqp2uPyNUE7W2Y00yExgQBWw.avif",
-    "vllh9TsMZorYx0kB8rUzIL9Jm8.avif"
-];
+// Function to open the lightbox with the clicked image
+function openLightbox(imageSrc) {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
 
-// Get elements
-const gallery = document.getElementById("gallery");
-const popup = document.getElementById("popup");
-const popupImg = document.getElementById("popup-img");
+    lightbox.style.display = "block";
+    lightboxImg.src = imageSrc;
+}
 
-// Load images into gallery
-images.forEach(image => {
-    const imgElement = document.createElement("img");
-    imgElement.src = `images/${image}`;
-    imgElement.alt = "Gallery Image";
-    imgElement.classList.add("gallery-img");
-
-    // Open popup on click
-    imgElement.addEventListener("click", () => {
-        popup.style.display = "flex";
-        popupImg.src = imgElement.src;
-    });
-
-    gallery.appendChild(imgElement);
-});
-
-// Close popup when clicking the X
-function closePopup() {
-    popup.style.display = "none";
+// Function to close the lightbox
+function closeLightbox() {
+    const lightbox = document.getElementById("lightbox");
+    lightbox.style.display = "none";
 }
