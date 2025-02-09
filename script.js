a238b6cfb3c600a5ff778e41f4c050f3a400fdba
stuxfian <116228096+ducks-quacky@users.noreply.github.com>
2025-02-09 11:36:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// List of image file names (Manually added)
+// List all images manually (GitHub Pages does not allow auto-fetching)
 const images = [
     "image1.avif",
-    "image2.jpg",
-    "image3.jpg",
-    "image4.jpg"
+    "mN1VKT3WvGehYiKDHY8n4BU.avif",
+    "q1gHqp2uPyNUE7W2Y00yExgQBWw.avif",
+    "vllh9TsMZorYx0kB8rUzIL9Jm8.avif"
 ];
 
 // Get the gallery div
@@ -12,7 +12,7 @@ const gallery = document.getElementById("gallery");
 // Loop through images and add them to the gallery
 images.forEach(image => {
     const imgElement = document.createElement("img");
-    imgElement.src = `/images/${image}`; // Path to images folder
+    imgElement.src = `images/${image}`; // Corrected path
     imgElement.alt = "Gallery Image";
     imgElement.classList.add("gallery-img");
     gallery.appendChild(imgElement);
